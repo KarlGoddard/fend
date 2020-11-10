7c2d80a8f41d67b2e9ed6d6b46500347e26e17de
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-10 20:15:28 +0000

    feat: add bodyParser and cors require statements.  Set placeholder for post request

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,35 @@
 var path = require('path')
-const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const express = require('express')
 
 const app = express()
-
 app.use(express.static('dist'))
 
-console.log(__dirname)
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+const cors = require('cors')
+app.use(cors())
+
+console.log(__dirname)
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.post('/tbc', xxx)
+
+function xxx(req, res) {
+    res.send(zzz)
 })
