2e34ea5150e6917a75b98cc8bd7541efd9fd8603
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-10 20:46:38 +0000

    feat: change port number for the express server

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -25,8 +25,8 @@ const mckey = `?key=${apikey}&of=json&lang=auto&txt=`
 const mctext = 'text input into html page'
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081')
 })
 
 app.get('/', function (req, res) {
