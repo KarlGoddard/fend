de7f2b4973d1886b5b686485a4ffdd568a3a807e
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-11 13:00:04 +0000

    feat: expermenting with getting the form to become a text box and bringing different elements in to the innerHTML

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -2,14 +2,17 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('txt').value
+
     Client.checkForName(formText)
+
+    document.getElementById('results').innerHTML = 'Your input was ' + formText
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results1').innerHTML = res.message
     })
 }
 
