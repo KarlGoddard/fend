eb37c38f33506463880e585310529521ee4fdbf8
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-11 20:01:51 +0000

    feat: first attempt at test call of post call in formHandler.js

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -6,10 +6,16 @@ function handleSubmit(event) {
 
     Client.checkForName(formText)
 
-    document.getElementById('results').innerHTML = 'Your input was ' + formText
+    //document.getElementById('results').innerHTML = 'Your input was ' + formText
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.message
+    })
+
+    fetch('http://localhost:8081/tbc')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results1').innerHTML = res.message
