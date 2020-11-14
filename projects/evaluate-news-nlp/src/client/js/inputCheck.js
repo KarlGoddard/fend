6e84f888b9d79bd76f6e0e6b6af596ce0febb578
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-14 19:09:48 +0000

    feat: several .scss changes to prepare for posting results in a grid layout

--- projects/evaluate-news-nlp/src/client/js/inputCheck.js
+++ projects/evaluate-news-nlp/src/client/js/inputCheck.js
@@ -1,16 +1,17 @@
 function inputChecker(url) {
     console.log("::: Running check valid input :::", url);
 
+//regcheck devised to ensure the url starts with http: or https:
     const regcheck = /^(http|https):/;
 
+//if passes regcheck, then the APIcall is made.
+//true or false also dictate the status or retry notification that is posted
     if(regcheck.test(url)) {
-        alert("it passes!");
-        return true;
+      return true;
     } else {
-      alert("it fails!");
       return false;
     }
-console.log('result was ' + regcheck.test(url));
+
 }
 
 export { inputChecker }
