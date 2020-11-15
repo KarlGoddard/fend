9e5555ad84dfdfd78e717eeec6054d0cbaa7600b
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-15 23:13:06 +0000

    feat: modified testformHandler to add 2 new tests.

--- projects/evaluate-news-nlp/__test__/testformHandler.spec.js
+++ projects/evaluate-news-nlp/__test__/testformHandler.spec.js
@@ -1,10 +1,16 @@
-import { handleSubmit } from '../src/client/js/formHandler'
+import { scoreText } from '../src/client/js/formHandler'
 
 
-describe("Testing the submit functionality", () => {
+describe("Converting the score response letter to a text value", () => {
     // function is dependent on calling a field entry that will not be present when the test is invoked. So should return false.
-    test("Testing the handleSubmit() function", () => {
-
-           expect(handleSubmit()).toBe(false);
+    test("Testing the correct text value is returned for test letters", () => {
+        const scoreLetter = 'P+'
+           expect(scoreText(scoreLetter)).toBe('POSITIVE');
   })
+
+  test("Testing the correct text value is returned for test letters", () => {
+      const scoreLetter = 'N'
+         expect(scoreText(scoreLetter)).toBe('NEGATIVE');
+})
+
 });
