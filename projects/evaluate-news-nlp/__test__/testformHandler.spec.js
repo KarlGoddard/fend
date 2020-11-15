cb430f1a73bd50a36c420fbcf07ffba4ee3d9b1b
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-15 20:43:29 +0000

    feat: add second test.js for formHandler.  Have added a return true and return false to the main function in order to prove the test.

--- projects/evaluate-news-nlp/__test__/testformHandler.spec.js
+++ projects/evaluate-news-nlp/__test__/testformHandler.spec.js
@@ -1,1 +1,10 @@
 import { handleSubmit } from '../src/client/js/formHandler'
+
+
+describe("Testing the submit functionality", () => {
+    // function is dependent on calling a field entry that will not be present when the test is invoked. So should return false.
+    test("Testing the handleSubmit() function", () => {
+
+           expect(handleSubmit()).toBe(false);
+  })
+});
--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -6,12 +6,7 @@ function handleSubmit(event) {
     let formText = document.getElementById("urlinput").value;
 
     if (Client.inputChecker(formText)) {
-    document.getElementById('outcome').innerHTML = 'Success.  Please wait while we retrieve the analysis';
-        // fetch('http://localhost:8081/test')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     document.getElementById('results').innerHTML = res.message
-    // })
+    document.getElementById('outcome').innerHTML = 'Valid url.  Please wait while we retrieve the analysis';
 
         fetch('http://localhost:8081/analysis',{
         method: "POST",
@@ -26,13 +21,15 @@ function handleSubmit(event) {
             document.getElementById('score').innerHTML = res.score_tag;
             document.getElementById('agreement').innerHTML = res.agreement;
             document.getElementById('subjectivity').innerHTML = res.subjectivity;
-            document.getElementById('confidence').innerHTML = res.confidence + '%';
+            document.getElementById('confidence').innerHTML = res.confidence;
             document.getElementById('irony').innerHTML = res.irony;
             document.getElementById('outcome').innerHTML = 'Analysis Complete';
+            return true;
           });
 
     } else {
     document.getElementById('outcome').innerHTML = 'Unable to analysis the url provided.  Please retry your entry ensuring it is a valid url starting with http: or https:';
+    return false;
     }
 
 }
