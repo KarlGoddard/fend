63009ea12708f4e3073b84863330f5387007ad08
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-15 19:50:34 +0000

    feat: first draft of inputCheck checker .js

--- projects/evaluate-news-nlp/__test__/testinputCheck.spec.js
+++ projects/evaluate-news-nlp/__test__/testinputCheck.spec.js
@@ -1,1 +1,25 @@
 import { inputChecker } from '../src/client/js/inputCheck'
+
+
+describe("Testing the regex check", () => {
+
+    test("Testing only true if url starts with uninterrupted sequence of characters = http: or https:", () => {
+           const urla = 'http://www.bbc.co.uk';
+           expect(inputChecker(urla)).toBe(true);
+         });
+
+     test("Testing only true if url starts with uninterrupted sequence of characters = http: or https:", () => {
+            const urlb = 'hthtp://www.bbc.co.uk';
+            expect(inputChecker(urlb)).toBe(false);
+          });
+
+    test("Testing only true if url starts with uninterrupted sequence of characters = http: or https:", () => {
+           const urlc = 'https://www.bbc.co.uk';
+           expect(inputChecker(urlc)).toBe(true);
+         });
+
+     test("Testing only true if url starts with uninterrupted sequence of characters = http: or https:", () => {
+            const urld = 'https//www.bbc.co.uk';
+            expect(inputChecker(urld)).toBe(false);
+          });
+});
